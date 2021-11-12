28d4a55f84ef4e3187d3ee3b65f4ea879bac0ba1
Vaibhav Narkhede <Vaibhav.Narkhede@outlook.com>
2021-11-12 23:55:39 +0530

    Added while & for loop example

---
+++ basic/for_loop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[]){
+    printf("for loop demo\n");
+    for (int i = 0; i < 10; i++){
+        printf("Hello World %d\n",i);
+    }
+    return 0;
+}
---
+++ basic/while_loop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char *argv[]){
+    printf("while loop demo\n");
+    int i = 0;
+    while (i < 10){
+        printf("Hello World %d\n",i);
+        i++;
+    }
+    return 0;
+}
